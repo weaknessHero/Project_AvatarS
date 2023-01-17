c57ccf987b41abedb24f918077558ca40631b65d
LeeJaeHyun <xleex9843@naver.com>
2023-01-18 00:33:15 +0900

    인공지능 업데이트및 폴더 세분화
    문서 폴더 추가

--- AIBuilder/test2AI.py
+++ AIBuilder/test2AI.py
@@ -1,3 +1,30 @@
+"""
+완료
+1번 배경 제거 
+
+진행
+2번 옷의 구김 제거
+- 옷의 메인컬러보다. 일정하게 RGB값이 높은 값은 제거(그림자 혹은 주름)
+3번 옷의 색상 데이터를 받는다.
+3-1번 많이 쓰이는 색상을 통해서 옷을 전체적으로 인식 시키는 방법 (퀄리티 낮음 난이도 쉬움, 데이터 적어도 됨) 
+3-2번 기본 옷의 셰이프를 인식해서 남는 부분을 맞추는 방법(퀄리티 높음 난이도 높음 데이터 많이 필요)
+3번 디자인 포지션을 인식하고, 색상이나 포켓등의 디자인을 받아올 작업 실시
+4번 받아온 값을 통해서 옷에 재배치 실시 
+5번 렌더링 실시
+
+
+
+
+"""
+
+
+"""
+선행작업
+pip install [numpy, opencv-python, matplotlib ]
+"""
+
+
+
 
 import time
 import io
@@ -5,6 +32,39 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os
 import requests
+import cv2
+
+
+
+import tensorflow as tf
+
+
+import tensorflow_datasets as tfds
+from tensorflow_examples.models.pix2pix import pix2pix
+
+import os
+import time
+import matplotlib.pyplot as plt
+from IPython.display import clear_output
+
+AUTOTUNE = tf.data.AUTOTUNE
+
+
+
+"""
+cycleGAN
+https://www.youtube.com/watch?v=Fkqf3dS9Cqw
+
+X 도메인 데이터와 Y 도메인 데이터를 통해서 
+두 도메인 간의 이미지 변환하는 법 학습
+
+
+목적 이미지를 인식 받아서 데이터 정보로 출력하는 방향을 목적으로 한다. 
+
+ input : 의상 이미지
+ output : 3D 모델에 입히기 적합한 데이터셋
+  
+"""
 
 
 from PIL import Image, ImageFile
@@ -13,30 +73,46 @@ from rembg.bg import remove as remove_bg
 
 
 
+#배경 제거
 def RemoveBackground(file_path):
+    orig_img = Image.open(file_path)
+      
+    #이미지 확인용
     fig = plt.figure(figsize=(10, 10))
-    # show original image
     fig.add_subplot(1, 2, 1)
-    orig_img = Image.open(file_path)
-    
-    # show bg removed image
-    orig_img = Image.open(file_path)
+    plt.imshow(orig_img)
 
+    #이간 측정
     started = time.time()
+    
+    #백그라운드 제거
     result = remove_bg(orig_img)
+       
+    #이전 이미지 확인용 
+    fig.add_subplot(1, 2, 2)
+    plt.imshow(result)
+    plt.show()
+
+    #시간 측정
     elapsed = time.time() - started
     print(f'it takes {elapsed} seconds for removing bg.')
-    result.show()
+
+    return np.asarray(result) # a는 읽기 전용입니다. 
+    
+"""
+단순화
+그림자 주름등의 렌더링에 불필요한 데이터 제거
+""" 
+
 
 
 dir_path="./Resource/TestIMG"
 
-
-
-
 #재귀탐색 폴더 내부의 폴더와 이미지 색인
 for (root, directories, files) in os.walk(dir_path):
     for file in files:
+        #이미지 주소 받기
         file_path = os.path.join(root, file)
         #배경 제거 프로세스
-        RemoveBackground(file_path)
+        IMG = RemoveBackground(file_path)
+        Simplification(IMG)
--- AIBuilder/testofAi.py
+++ AIBuilder/testofAi.py
@@ -1,98 +1,16 @@
-"""
-
-옷을 선정하여 누끼를 따는 작업 실시
-
-1번 옷을 인지한다.
-2번 옷의 색상 데이터를 받는다.
-
-
-1번 구겨진 상태를 받아서 인식하는 방법(퀄리티 높음 난이도 어려움) 
-2번 기본 옷의 셰이프를 인식해서 남는 부분을 맞추는 방법(퀄리티 중간 난이도 중간)
-안보이는 디자인 부분을 받을 방법생각
-유사 이미지 셋으로 받는등의 방식
-3번 디자인 포지션을 인식하고, 색상이나 포켓등의 디자인을 받아올 작업 실시
-4번 받아온 값을 통해서 옷에 재배치 실시 
-5번 렌더링 실시
-
-
-현재 문제점
- 배경과 객체를 구분하지 못한다. 
-
-"""
-
-
-"""
-선행작업
-pip install [numpy, opencv-python, matplotlib ]
-"""
-
-
+import pandas as pd
 import numpy as np
-import cv2
-from matplotlib import pyplot as plt
+
+import tensorflow as tf
+
+
+import tensorflow_datasets as tfds
+from tensorflow_examples.models.pix2pix import pix2pix
+
 import os
+import time
+import matplotlib.pyplot as plt
+from IPython.display import clear_output
 
-
-"""
-
-배경 이미지 제거 (누끼작업 )
-
-img - 입력 이미지
-mask - 배경, 전경 또는 가능한 배경/전경 등의 영역을 지정하는 마스크 이미지입니다. cv2.GC_BGD, cv2.GC_FGD, cv2.GC_PR_BGD, cv2.GC_PR_FGD 플래그로 수행 하거나 단순히 전달합니다. 0,1,2,3 이미지.
-rect - (x,y,w,h) 형식의 전경 개체를 포함하는 사각형의 좌표입니다.
-bdgModel , fgdModel - 알고리즘이 내부적으로 사용하는 배열입니다. 크기가 (1,65)인 두 개의 np.float64 유형 0 배열을 생성하기만 하면 됩니다.
-iterCount - 알고리즘이 실행해야 하는 반복 횟수입니다.
-모드 - cv2.GC_INIT_WITH_RECT 또는 cv2.GC_INIT_WITH_MASK 이거나 사각형을 그릴지 아니면 마지막 터치업 스트로크를 그릴지 결정하는 조합이어야 합니다.
-"""
-
-
-
-
-
-
-def RemoveBackground(file_path):
-    print(file_path)
-    #이미지 인식 Numpy array 형태 반환
-    img_BGR = cv2.imread(file_path)
-
-    #이미지 양식 BGR에서 RGB로 변경
-    img = cv2.cvtColor(img_BGR, cv2.COLOR_BGR2RGB)
-  
-    #이미지 사이즈인 Y*X 크기의 0 행렬 반환 uint8은 숫자 데이터 타입(양수 0~255)
-    mask = np.zeros(img.shape[:2],np.uint8)
-
-    #1*65 크기의 0 행렬 반환 float64데이터타입        
-    bgdModel = np.zeros((1,65),np.float64)
-    fgdModel = np.zeros((1,65),np.float64)
-
-    #시작점 x,y,width, height 
-    rect = (10,10,450,500)
-
-    #그랩컷 실행 1.원본 이미지, 2. 마스크. 3. 범위 지정 사각형 4. 배경용 임시배열 5. 전경용 임시배열 6. 반복 횟수 7번 사각형을 위한 초기화
-    cv2.grabCut(img,mask,rect,bgdModel,fgdModel,5,cv2.GC_INIT_WITH_RECT)
-
-
-    #배경 (2, 0)인곳은 0으로 그외에는 1로 설정한 마스크 생성
-    mask2 = np.where((mask==2)|(mask==0),0,1).astype('uint8')
-
-    #새 마스크를 통해 배경 제외
-    img = img*mask2[:,:,np.newaxis]
-
-
-    #쇼
-    plt.imshow(img),plt.colorbar(),plt.show()
-
-
-dir_path="./Resource/TestIMG"
-
-#재귀탐색 폴더 내부의 폴더와 이미지 색인
-for (root, directories, files) in os.walk(dir_path):
-    for file in files:
-        file_path = os.path.join(root, file)
-
-        #배경 제거 프로세스
-        RemoveBackground(file_path)
-
-
-
+AUTOTUNE = tf.data.AUTOTUNE
 
