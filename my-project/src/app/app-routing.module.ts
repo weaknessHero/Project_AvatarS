417fe6e668ae0aeb2ad930357a2563a75027dea5
HwangSedong <saedongh5@naver.com>
2023-01-31 00:55:53 +0900

    상품 노출 하드 코딩
    상품 노출 하드 코딩

--- my-project/src/app/app-routing.module.ts
+++ my-project/src/app/app-routing.module.ts
@@ -5,12 +5,14 @@ import {SigninComponent} from "./signin/signin.component";
 import {SignupComponent} from "./signup/signup.component";
 import {UserinfoComponent} from "./userinfo/userinfo.component";
 import {MainComponent} from "./main/main.component";
+import {ProductListComponent} from "./product-list/product-list.component";
 
 const routes: Routes = [
   { path: 'signin', component: SigninComponent},
   { path: 'signup', component: SignupComponent},
   { path: 'userinfo', component: UserinfoComponent},
   { path: 'main', component: MainComponent},
+  { path: 'ProductList', component: ProductListComponent},
 //  {path: 'login', component: LoginComponent}
 ];
 
--- my-project/src/app/product-list/product-list.component.ts
+++ my-project/src/app/product-list/product-list.component.ts
@@ -13,19 +13,50 @@ export class ProductListComponent implements OnInit{
   constructor(private http: HttpClient) {}
 
   products: Product[] = [{
+    image: '../../assets/img/1.png',
     pid: '1',
-    pname: '헤링본 자켓',
-    price: 30000,
+    pname: '스프링 울 2-WAY 헤링턴 자켓',
+    price: 111585,
     rating: 3.3,
-    brand: '라퍼지스토어',
+    brand: '키뮤어',
+    type: '아우터',
     style: ['캐주얼', '아메카지']
   },{
+    image: '../../assets/img/2.png',
     pid: '2',
-    pname: '블루 바시티',
-    price: 50000,
+    pname: '어센틱 로고 후디',
+    price: 68671,
     rating: 4.3,
-    brand: 'EURL',
-    style: ['캐주얼', '시티보이']
+    brand: '커버낫',
+    type: '상의',
+    style: ['캐주얼']
+  },{
+    image: '../../assets/img/3.png',
+    pid: '3',
+    pname: '리버시블 슬랙스',
+    price: 32000,
+    rating: 2.7,
+    brand: '후러브스아트',
+    type: '바지',
+    style: ['아메카지']
+  },{
+    image: '../../assets/img/4.png',
+    pid: '4',
+    pname: '로고 패치 캐시미어 머플러',
+    price: 898000,
+    rating: 3.7,
+    brand: '질 샌더',
+    type: '악세사리',
+    style: ['럭셔리']
+  },{
+    image: '../../assets/img/5.png',
+    pid: '5',
+    pname: '4.5cm 소가죽 코만도 첼시부츠 루틴(CR0011)',
+    price: 86800,
+    rating: 3.4,
+    brand: 'CUSTOMADE',
+    type: '악세사리',
+    style: ['신발']
   }];
   ngOnInit(): void {
   }
@@ -33,10 +64,12 @@ export class ProductListComponent implements OnInit{
 
 
 export class Product{
+  image: any;
   pid: string;
   pname: string;
   price: number;
   rating: number;
   brand: string;
+  type: string;
   style: string[];
 }
