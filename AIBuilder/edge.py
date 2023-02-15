0d5e422369d754fb651a81ae25312ab97f34d418
LeeJaeHyun <xleex9843@naver.com>
2023-02-16 01:22:02 +0900

    a
    a

--- AIBuilder/edge.py
+++ AIBuilder/edge.py
@@ -8,6 +8,15 @@ Created on Wed Jan 18 02:59:24 2023
 이미지 컨투어?
 
 
+초기 데이터 2D 옷 사진
+
+
+3D 모델에 맞는 의상을 위한 텍스쳐
+
+목적 3D 모델에 맞는 의상 목표 
+
+
+
 
 """
 
@@ -16,45 +25,26 @@ import cv2
 import numpy as np
 
 img = cv2.imread("../Resource/AIInputIMG/티셔츠/903340_7_220.jpg")
-img2 = img.copy()
+
 # 바이너리 이미지로 변환
-imgray = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
-ret, th = cv2.threshold(imgray, 127,255,cv2.THRESH_BINARY_INV)
+imgray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-# 컨튜어 찾기
-contours, hr = cv2.findContours(th, cv2.RETR_EXTERNAL, \
-                                        cv2.CHAIN_APPROX_SIMPLE)
-contr = contours[0]
+ret, imthres  = cv2.threshold(imgray, 127,255,cv2.THRESH_BINARY_INV)
+# 가장 바깥쪽 컨투어에 대해 모든 좌표 반환 ---③
+contour, hierachy = cv2.findContours(imthres, cv2.RETR_EXTERNAL, \
+                                                 cv2.CHAIN_APPROX_NONE)
 
-# 감싸는 사각형 표시(검정색)
-x,y,w,h = cv2.boundingRect(contr)
-cv2.rectangle(img, (x,y), (x+w, y+h), (0,0,0), 3)
 
-# 최소한의 사각형 표시(초록색)
-rect = cv2.minAreaRect(contr)
-box = cv2.boxPoints(rect)   # 중심점과 각도를 4개의 꼭지점 좌표로 변환
-box = np.int0(box)          # 정수로 변환
-cv2.drawContours(img, [box], -1, (0,255,0), 3)
+    
+print(contour)    
 
-# 최소한의 원 표시(파랑색)
-(x,y), radius = cv2.minEnclosingCircle(contr)
-cv2.circle(img, (int(x), int(y)), int(radius), (255,0,0), 2)
+     
+for cnt in contour:    
+    # 모든 좌표를 갖는 컨투어 그리기, 초록색  ---⑥
+    cv2.drawContours(img, [cnt], -1, (0,255,0), 4)
 
-# 최소한의 삼각형 표시(분홍색)
-ret, tri = cv2.minEnclosingTriangle(contr)
-cv2.polylines(img, [np.int32(tri)], True, (255,0,255), 2)
 
-# 최소한의 타원 표시(노랑색)
-ellipse = cv2.fitEllipse(contr)
-cv2.ellipse(img, ellipse, (0,255,255), 3)
-
-# 중심점 통과하는 직선 표시(빨강색)
-[vx,vy,x,y] = cv2.fitLine(contr, cv2.DIST_L2,0,0.01,0.01)
-cols,rows = img.shape[:2]
-cv2.line(img,(int(0, 0-x*(vy/vx) + y)), (cols-1, (cols-x)*(vy/vx) + y), \
-                                                        (0,0,255),2)
-
-# 결과 출력
-cv2.imshow('Bound Fit shapes', img)
+# 결과 출력 ---⑩
+cv2.imshow('result', img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
