49ce10fa8ceb7ba3f60b48a35236ab356f5b1886
LeeJaeHyun <xleex9843@naver.com>
2023-02-16 11:26:52 +0900

    api123 삭제

--- my-project2/api123/src/main/java/project/avatar/api/repo/UserJpaRepo.java
+++
@@ -1,7 +0,0 @@
-package project.avatar.api.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import project.avatar.api.entity.Users;
-
-public interface UserJpaRepo extends JpaRepository<Users, Long> {
-}
