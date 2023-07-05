e48aeede031de1cc1cd2c91d1881f68d2fcba76a
HwangSedong <saedongh5@naver.com>
2023-07-05 23:25:57 +0900

    7/5

--- my-project2/api/src/main/java/project/avatar/api/controller/products/ImageController.java
+++ my-project2/api/src/main/java/project/avatar/api/controller/products/ImageController.java
@@ -1,16 +1,22 @@
 package project.avatar.api.controller.products;
 
-import lombok.Value;
 import org.springframework.core.io.UrlResource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
+import org.springframework.util.ResourceUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import javax.annotation.Resource;
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.file.Files;
 
 @RestController
@@ -18,8 +24,49 @@ import java.nio.file.Files;
 @Component
 public class ImageController {
 
+    @GetMapping("/generate")
+    public ResponseEntity<byte[]> generateImage() throws IOException {
+        // 이미지 생성 로직
+        File imageFile = createMergedImage();
+
+        // 파일을 읽어서 byte 배열로 변환
+        byte[] imageBytes = readImageBytes(imageFile);
+
+        // 파일 다운로드를 위한 응답 헤더 설정
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentDispositionFormData("attachment", "merged_image.png");
+        headers.setContentType(MediaType.APPLICATION_OCTET_STREAM);
+
+        // 이미지 파일과 헤더를 포함하는 응답 반환
+        return ResponseEntity.ok()
+                .headers(headers)
+                .body(imageBytes);
+    }
+
+    private File createMergedImage() throws IOException {
+        int width = 300;
+        int height = 250;
+        // 이미지 생성 로직 작성
+        BufferedImage mergedImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+        // 이미지 그리기
+        Graphics2D graphics = mergedImage.createGraphics();
+        // ...
+
+        // 이미지 파일로 저장
+        File imageFile = new File("merged_image.png");
+        ImageIO.write(mergedImage, "png", imageFile);
+
+        return imageFile;
+    }
+
+    private byte[] readImageBytes(File file) throws IOException {
+        try (InputStream inputStream = new FileInputStream(file)) {
+            return inputStream.readAllBytes();
+        }
+    }
+
     //@Value("${image.storage.path}")
-    private String imageStoragePath;
+    /*private String imageStoragePath;
 
     @PostMapping
     public String uploadImage(@RequestParam("file") MultipartFile file) throws IOException{
@@ -40,5 +87,5 @@ public class ImageController {
         return ResponseEntity.ok()
                 .contentType(MediaType.parseMediaType(Files.probeContentType(file.toPath())))
                 .body(resource);
-    }
+    }*/
 }
--- my-project2/frontend/src/app/component/fit/fit-main/fit-main.component.ts
+++ my-project2/frontend/src/app/component/fit/fit-main/fit-main.component.ts
@@ -7,9 +7,16 @@ import { Component, ElementRef, ViewChild } from '@angular/core';
 })
 export class FitMainComponent {
 
-  imagePath: string;
-  selectedColor: string;
+  imagePath: string = '/assets/avatar_woman.png';
+  selectedTopColor: string;
+  selectedTopCategory: string;
+  selectedBottomColor: string;
+  selectedBottomCategory: string;
+  overlayImg: HTMLImageElement = new Image();
   @ViewChild('imageElement') imageElement: ElementRef;
+  @ViewChild('previewImage', { static: false }) previewImage: ElementRef;
+  @ViewChild('overlayImage') overlayImage: ElementRef;
+  @ViewChild('canvas', { static: false }) canvas: ElementRef;
 
   zoomIn($event: MouseEvent){
     const target = $event.currentTarget as HTMLElement;
@@ -30,7 +37,7 @@ export class FitMainComponent {
     this.imagePath = '/assets/avatar_woman.png'
   }
 
-  handleCategoryChange(event: any) {
+  /*handleCategoryChange(event: any) {
     const selectedCategory = event.target.value;
     // 선택된 카테고리를 처리하는 로직을 구현합니다.
     console.log('선택된 카테고리:', selectedCategory);
@@ -47,11 +54,36 @@ export class FitMainComponent {
     } else if (this.selectedColor === 'top_color5') {
       document.querySelector('select').style.color = 'grey';
     }
-  }
+  }*/
 
   openImageInput() {
     const imageInput = document.getElementById('image-input') as HTMLInputElement;
     imageInput.click();
+  }
+
+  /*handleColorChange(event: any){
+    if(this.selectedColor==='top_color1'){
+      //빨간 색 선택 시
+      this.previewImage.nativeElement.style.backgroundImage = 'url("/assets/red_tshirt.png")';
+    } else if(this.selectedColor==='top_color2'){
+      //파란색 선택 시
+      this.previewImage.nativeElement.style.backgroundImage = 'url("/assets/blue_tshirt.png")';
+    } else if(this.selectedColor==='top_color3'){
+      //초록색 선택 시
+      this.previewImage.nativeElement.style.backgroundImage = 'url("/assets/green_tshirt.png")';
+    } else if(this.selectedColor==='top_color4'){
+      //검정색 선택 시
+      this.previewImage.nativeElement.style.backgroundImage = 'url("/assets/black_tshirt.png")';
+    } else if(this.selectedColor==='top_color5'){
+      //흰색 선택 시
+      this.previewImage.nativeElement.style.backgroundImage = 'url("/assets/white_tshirt.png")';
+    }
+  }*/
+
+  removePreviewImage(){
+    const previewImage = this.previewImage.nativeElement;
+    previewImage.src = '';
+    previewImage.style.display = 'none';
   }
 
   handleImageChange(event: any) {
@@ -74,5 +106,69 @@ export class FitMainComponent {
       previewImage.src = '';
       previewImage.style.display = 'none';
     }
+  }
+
+  downloadImage(): void {
+    const avatarImage = document.getElementById('avatar-image') as HTMLImageElement;
+    const overlayImage = document.getElementById('overlay-image') as HTMLImageElement;
+
+    avatarImage.onload = () => {
+      const canvas = document.createElement('canvas');
+      const context = canvas.getContext('2d');
+
+      canvas.width = avatarImage.width;
+      canvas.height = avatarImage.height;
+
+      // 아바타 이미지 그리기
+      context.drawImage(avatarImage, 0, 0);
+
+      // 오버레이 이미지 그리기
+      context.drawImage(overlayImage, 0, 0);
+
+      // 캔버스 데이터를 이미지 URL로 변환하여 다운로드
+      const dataURL = canvas.toDataURL();
+      const link = document.createElement('a');
+      link.href = dataURL;
+      link.download = 'merged_image.png';
+
+      // 다운로드 트리거
+      window.location.href = link.href;
+    };
+  }
+
+
+
+  handleTopCategoryChange(event: any) {
+    // 선택된 상의 종류 변경 처리
+    this.selectedTopCategory = event.target.value;
+    // 추가적인 로직 수행
+  }
+
+  handleTopColorChange(event: any) {
+    // 선택된 상의 색상 변경 처리
+    this.selectedTopColor = event.target.value;
+    // 추가적인 로직 수행
+  }
+
+  handleBottomCategoryChange(event: any) {
+    // 선택된 하의 종류 변경 처리
+    this.selectedBottomCategory = event.target.value;
+    // 추가적인 로직 수행
+  }
+
+  handleBottomColorChange(event: any) {
+    // 선택된 하의 색상 변경 처리
+    this.selectedBottomColor = event.target.value;
+    // 추가적인 로직 수행
+  }
+
+  handleShoesCategoryChange(event: any) {
+    // 선택된 신발 종류 변경 처리
+    // 추가적인 로직 수행
+  }
+
+  handleSexCategoryChange(event: any) {
+    // 선택된 성별 변경 처리
+    // 추가적인 로직 수행
   }
 }
