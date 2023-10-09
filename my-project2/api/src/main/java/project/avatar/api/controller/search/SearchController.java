b1b5fe135e0b297a7d1daa58fb73d82ec763181b
HwangSedong <saedongh5@naver.com>
2023-10-10 00:34:37 +0900

    Update SearchController.java

--- my-project2/api/src/main/java/project/avatar/api/controller/search/SearchController.java
+++ my-project2/api/src/main/java/project/avatar/api/controller/search/SearchController.java
@@ -1,8 +1,13 @@
 package project.avatar.api.controller.search;
 
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
@@ -25,7 +30,7 @@ public class SearchController {
         HttpEntity<String> entity = new HttpEntity<>(headers);
 
         Object apiResults = restTemplate.exchange(
-                "https://openapi.naver.com/v1/search/shop.json?query=" + query,
+                "https://openapi.naver.com/v1/search/shop.json?query=" + query + "&display=30",
                 HttpMethod.GET,
                 entity,
                 Object.class
@@ -33,4 +38,32 @@ public class SearchController {
 
         return apiResults;
     }
+    /*@GetMapping("/naverSearch")
+    public List<Object> search(@RequestParam String query){
+        RestTemplate restTemplate = new RestTemplate();
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("X-Naver-Client-Id", clientId);
+        headers.add("X-Naver-Client-Secret", clientSecret);
+
+        HttpEntity<String> entity = new HttpEntity<>(headers);
+
+        ResponseEntity<Map> responseEntity = restTemplate.exchange(
+                "https://openapi.naver.com/v1/search/shop.json?query=" + query + "&display=100",
+                HttpMethod.GET,
+                entity,
+                Map.class
+            );
+
+        Map<String,Object> apiResults = responseEntity.getBody();
+
+        // Get items from the result
+        List<Map<String,Object>> items = (List<Map<String,Object>>)apiResults.get("items");
+
+        // Filter the items where category1 is 패션의류 and limit the results to 12
+        return items.stream()
+            .filter(item -> "패션의류".equals(item.get("category1")))
+            .limit(12)
+            .collect(Collectors.toList());
+    }*/
 }
